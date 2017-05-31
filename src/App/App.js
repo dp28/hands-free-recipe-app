2e04f05767a5d45d456958028f1d65f4eefd8e42
Daniel Patterson <dan24patt@gmail.com>
2017-05-31 08:21:43 +0100

    Move App into own directory

---
+++ src/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <div className="App-header">
+      <h2>Welcome to React</h2>
+    </div>
+  </div>
+);
+
+export default App;
